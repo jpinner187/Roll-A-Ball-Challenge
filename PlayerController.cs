eec36b5800061fd4591eff604553ca29a0b7f6af
jpinner187 <46573272+jpinner187@users.noreply.github.com>
2019-02-10 17:31:27 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text scoreText;
+
+    private Rigidbody rb;
+    private int count;
+    private int lives;
+    private int score;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        lives = 3;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetCountText();
+
+        }
+
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+            lives = lives - 1;
+            score = score - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Score" + score.ToString();
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+           transform.position = new Vector3(16.72f, .5f, 18.07f);
+        }
+        if (score >= 24)
+        {
+         winText.text = "You Win!";
+
+        }
+       
+
+    }
+}
